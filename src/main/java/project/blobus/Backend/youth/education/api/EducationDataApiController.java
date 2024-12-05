b9889513bf7bed63bd84f105f50d6777dc9c2330
Cojeb2 <willietoy@naver.com>
2024-12-05 09:37:43 +0900

    ver1에 대해서 êµ서 기능, 데이터 부분은 최종 정리됨

--- src/main/java/project/blobus/Backend/youth/education/api/EducationDataApiController.java
+++ src/main/java/project/blobus/Backend/youth/education/api/EducationDataApiController.java
@@ -13,6 +13,9 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 //온통청년 API를 사용해서 지역 : 부산, 카테고리 : 교육으로 데이터 가져오는 로직
 @RestController
 @RequiredArgsConstructor
@@ -52,13 +55,6 @@ public class EducationDataApiController {
             XmlMapper xmlMapper = new XmlMapper();
             JsonNode jsonNode = xmlMapper.readTree(response.getBytes());
 
-            // JSON 데이터를 DTO로 변환하고 서비스 계층을 통해 저장
-            // 저장되는 로직 추가는 했는데 아직 안돌려봄.
-            // 즉, 코드는 짰는데 동작은 안시켜봄. 나중에 동작해봐야함
-            // 꼭 주석풀고 해봐야됨!!!!
-//            EducationDataApiDTO educationDataApiDTO = new ObjectMapper().treeToValue(jsonNode, EducationDataApiDTO.class);
-//            educationDataApiService.savePolicy(educationDataApiDTO);
-
             // youthPolicy 배열 추출
             JsonNode youthPolicyArray = jsonNode.get("youthPolicy");
 
@@ -66,6 +62,7 @@ public class EducationDataApiController {
             ObjectMapper objectMapper = new ObjectMapper();
             for (JsonNode policyNode : youthPolicyArray) {
                 EducationDataApiDTO educationDataApiDTO = objectMapper.treeToValue(policyNode, EducationDataApiDTO.class);
+
                 educationDataApiService.savePolicy(educationDataApiDTO);
             }
 
@@ -75,7 +72,9 @@ public class EducationDataApiController {
         } catch (Exception e) {
             e.printStackTrace();
             // 예외 발생 시 에러 메시지 반환
-            return ResponseEntity.status(500).body("Error fetching finance policy data: " + e.getMessage());
+            return ResponseEntity.status(500).body("Error fetching welfare policy data: " + e.getMessage());
         }
     }
+
+
 }
