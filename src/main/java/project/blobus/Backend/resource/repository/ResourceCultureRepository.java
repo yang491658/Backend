c6ffbcf90a7d755b140be2c59b882c8b4a4bbd0f
yang4916 <park49164357@gmail.com>
2024-12-11 17:26:55 +0900

    241211 1726

--- src/main/java/project/blobus/Backend/resource/repository/ResourceCultureRepository.java
+++ src/main/java/project/blobus/Backend/resource/repository/ResourceCultureRepository.java
@@ -3,6 +3,10 @@ package project.blobus.Backend.resource.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import project.blobus.Backend.resource.entity.ResourceCulture;
 
+import java.util.Optional;
+
 public interface ResourceCultureRepository extends JpaRepository<ResourceCulture, Long> {
     boolean existsByResNum(Long resNum);
+
+    Optional<ResourceCulture> findByResNum(Long resNum);
 }
