fa8a7468b56a536a1523ca0aa21f869a6d4dbdbc
jun <grow9818@naver.com>
2024-12-13 14:25:41 +0900

    12월13일

--- src/main/java/project/blobus/Backend/main/dto/AllSearchDTO.java
+++ src/main/java/project/blobus/Backend/main/dto/AllSearchDTO.java
@@ -2,4 +2,8 @@ package project.blobus.Backend.main.dto;
 
 public interface AllSearchDTO {
     String getTitle();
+    Long getId();
+    String getCategory();
+    boolean getVisibility();
+    String getAuthorId();
 }
